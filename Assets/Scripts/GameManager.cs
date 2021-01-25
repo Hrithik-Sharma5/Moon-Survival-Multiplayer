4a590b6414454f3f5989b692e6212a6fa2971686
Hrithik Sharma <hrithik5374@gmail.com>
2021-01-25 05:32:10 +0530

    Multiplayer Lobby progress 40%

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,14 @@ public class GameManager : MonoBehaviour
     public Transform[] spawnPos;
     public GameObject enemy;
     public Transform enemyContainer;
+
+    [SerializeField]private GameObject projectileContainer;
+    [SerializeField]private GameObject shootParticleContainer;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private GameObject shootParticlePrefab;
+    [SerializeField] private GameObject enemyPrefab;
+
+    List<GameObject> enemyList = new List<GameObject>(); 
 
     void Start()
     {
@@ -21,21 +30,53 @@ public class GameManager : MonoBehaviour
             instance = this;
         }
 
+        for (int i = 0; i < 25; i++)
+        {
+            PhotonNetwork.Instantiate(shootParticlePrefab.name, Vector3.zero,Quaternion.identity);
+            PhotonNetwork.Instantiate(projectilePrefab.name, Vector3.zero,Quaternion.identity);
+            PhotonNetwork.Instantiate(enemy.name, Vector3.zero, Quaternion.identity);
+        }
+
         foreach (var item in spawnPos)
         {
-            Instantiate(enemy, item.position, Quaternion.identity, enemyContainer);
+            enemyPrefab = GetEnemyPool();
+            if (enemyPrefab != null)
+            {
+                enemyPrefab.transform.position = item.position;
+                enemyPrefab.SetActive(true);
+                //Instantiate(enemy, spawnPos[randomPoint].position, Quaternion.identity, enemyContainer);
+            }
         }
+
         StartCoroutine(SpawnEnemy());
+
+
     }
 
     IEnumerator SpawnEnemy()
     {
+        GameObject enemyPrefab;
         while (true)
         {
             yield return new WaitForSeconds(10);
             int randomPoint = Random.Range(0, spawnPos.Length);
-            Instantiate(enemy, spawnPos[randomPoint].position, Quaternion.identity, enemyContainer);
+            enemyPrefab = GetEnemyPool();
+            if (enemyPrefab != null)
+            {
+                enemyPrefab.transform.position = spawnPos[randomPoint].position;
+                enemyPrefab.SetActive(true);
+                //Instantiate(enemy, spawnPos[randomPoint].position, Quaternion.identity, enemyContainer);
+            }
         }
+    }
+
+    GameObject GetEnemyPool()
+    {
+        foreach (var item in enemyList)
+        {
+            if (item.activeSelf) return item;
+        }
+        return null;
     }
 
 }
---
+++ Assets/Scripts/PhotonLobbyManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonLobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField playerNameInput;
+    [SerializeField] InputField roomNameInputField;
+    [SerializeField] GameObject startPanel;
+    [SerializeField] GameObject insideRoomPanel;
+    [SerializeField] GameObject createRoomPanel;
+    [SerializeField] GameObject roomListPanel;
+    [SerializeField] GameObject[] allPanels;
+
+    void Start()
+    {
+
+    }
+
+    #region Callbacks for buttons
+
+    public void OnEnterNameButton()
+    {
+        string playerName = playerNameInput.text;
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            PhotonNetwork.LocalPlayer.NickName = playerName;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public void OnCreateRoomButton()
+    {
+        string roomname = roomNameInputField.text;
+        RoomOptions roomOption = new RoomOptions();
+
+        if (!string.IsNullOrEmpty(roomname))
+        {
+            PhotonNetwork.CreateRoom(roomname, roomOption); 
+        }
+    }
+
+    #endregion
+
+    #region Photon Callbacks
+    public override void OnConnectedToMaster()
+    {
+        ActivatePanel(startPanel);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        ActivatePanel(insideRoomPanel);
+    }
+
+
+    #endregion
+
+    #region Helper Functions
+
+    private void ActivatePanel(GameObject panelToActivate)
+    {
+        foreach (GameObject item in allPanels)
+        {
+            if (item.name != panelToActivate.name)
+            {
+                item.SetActive(false);
+            }
+            else item.SetActive(true);
+        }
+    }
+
+
+    #endregion 
+
+}
